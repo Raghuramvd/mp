ec525aee6922cc91fc8054cfbee5ee0d66cbb47c
Elesssur <russell10mathias@gmail.com>
2022-01-10 16:51:18 +0530

    make it better

--- server.py
+++ server.py
@@ -2,8 +2,7 @@
 from flask import Flask, request, jsonify, render_template
 import util
 
-app = Flask(_name_,  template_folder="client",
-            static_folder="artifact", static_url_path="")
+app = Flask(_name_, static_folder="artifact", static_url_path="")
 
 
 @app.route('/')
